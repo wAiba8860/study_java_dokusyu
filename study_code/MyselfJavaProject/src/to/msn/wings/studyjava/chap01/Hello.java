58f2492bd48616aa833339d95383686349f254d6
wAiba8860 <wayjava03remi03@gmail.com>
2025-05-31 11:27:39 +0900

    note PC

--- study_code/MyselfJavaProject/src/to/msn/wings/studyjava/chap01/Hello.java
+++ study_code/MyselfJavaProject/src/to/msn/wings/studyjava/chap01/Hello.java
@@ -3,7 +3,7 @@ package to.msn.wings.studyjava.chap01;
 import java.time.LocalDateTime;
 
 public class Hello {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         LocalDateTime time = LocalDateTime.now();
         System.out.println(time);
     }
